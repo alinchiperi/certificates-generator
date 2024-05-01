f349e72c93d6c8e5a2864dfc4560e450d6dcb602
Alin Chiperi <chiperialin@yahoo.com>
2024-05-01 10:29:17 +0300

    Exception Handler

---
+++ src/main/java/ro/usv/certificates_generator/config/handler/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package ro.usv.certificates_generator.config.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseEntity<Object> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
